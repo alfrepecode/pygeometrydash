5e1a92d2a3130172bab8d98ba51900b4facc32df
alfrepecode <alfrepecode@gmail.com>
2024-02-29 18:26:39 +0100

    clase 4: utilizando grupos de sprites

--- game.py
+++ game.py
@@ -9,11 +9,13 @@ clock = pygame.time.Clock()
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, pos, width, height, img_path):
+        super().__init__()
         self.image = load_image(img_path, (width, height))
         self.rect = self.image.get_rect(topleft = pos)
 
-block = Block((0,0), 34, 34, os.path.join('imgs', 'block.png'))
-block2 = Block((34,0), 34, 34, os.path.join('imgs', 'block.png'))
+blocks = pygame.sprite.Group()
+blocks.add(Block((0,0), 34, 34, os.path.join('imgs', 'block.png')))
+blocks.add(Block((34,0), 34, 34, os.path.join('imgs', 'block.png')))
 
 while True:
     for event in pygame.event.get():
@@ -21,7 +23,6 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill('lightblue')
-    screen.blit(block.image, block.rect)
-    screen.blit(block2.image, block2.rect)
+    blocks.draw(screen)
     clock.tick(FPS)
     pygame.display.update()
