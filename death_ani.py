4dbac1b704dee86b34ea30c6b177a022398cafb3
alfrepecode <alfrepecode@gmail.com>
2024-02-29 18:26:41 +0100

    clase 39: probando la animacion

--- death_ani.py
+++ death_ani.py
@@ -18,13 +18,20 @@ class Death(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, (142, 142, 142, self.opacity),(self.image.get_width()//2, self.image.get_height()//2),self.radius)
         self.radius += 3
         self.opacity -= 6
+        if self.opacity <= 0 or self.radius >= self.image.get_width()//2:
+            self.kill()
         
 if __name__ == '__main__':
+    death_ani = pygame.sprite.GroupSingle()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                death_ani.add(Death(pygame.mouse.get_pos()))
         screen.fill('lightblue')
+        death_ani.update()
+        death_ani.draw(screen)
         clock.tick(FPS)
         pygame.display.update()
